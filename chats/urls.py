30999bde362ef1299a69be1bf3a657ab774bfa61
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-08-15 02:22:02 +0800

    login page

--- chats/urls.py
+++ chats/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.Index, name='index'),
     path("chat/<str:room_name>/", views.Room, name='room'),
+    # AUTH
+    path('login', views.Login, name='login'),
+    path('register', views.Register, name='register'),
+    path('logout/', views.logoutPage, name='logout'),
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
 import uuid
+
+from .forms import *
+
 
 # Create your views here.
 
 
+@login_required(login_url='login')
 def Index(request):
     randROOM = None
     if request.method == "POST":
@@ -11,5 +23,46 @@ def Index(request):
     return render(request, 'chats/dashboard.html', {'randROOM': randROOM})
 
 
+@login_required(login_url='login')
 def Room(request, room_name):
     return render(request, 'chats/room.html', {'room_name': room_name})
+
+
+def Login(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, "Username or Password is Incorrect")
+
+    context = {}
+    return render(request, 'chats/login.html')
+
+
+def Register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, "Account Created For " + user)
+                return redirect('login')
+
+    context = {"form": form}
+    return render(request, 'chats/register.html', context)
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
